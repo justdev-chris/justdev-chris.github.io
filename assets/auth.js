bb0fea71febd424f0b85643c737ce05880dbb446
Chris <morenochristopher580@gmail.com>
2025-08-05 17:20:12 -0500

    Update auth.js

--- assets/auth.js
+++ assets/auth.js
@@ -1,3 +1,4 @@
+// javascript for stupid password protected sites since yall tryna spy on me lol
 const dastupidsitethatspasswordprotected = window.location.pathname;
 window.location.href = '/login.html?redirect=${encodeURIComponent(dastupidsitethatspasswordprotected)}';
 
