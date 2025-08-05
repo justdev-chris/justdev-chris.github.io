afd4747b5167e0c5ac8267a4ffac5769526a84b8
Chris <morenochristopher580@gmail.com>
2025-08-05 17:29:15 -0500

    Update auth.js

--- assets/auth.js
+++ assets/auth.js
@@ -1,3 +1,4 @@
+// a lil js for protected password sites... dont even think about it.
 const panelpasswdidk123lol = "catboy:Bp5tkbgCj9iVtXuAU6Zb"; // noo dont even think about it
 
 const input = prompt("restricted page... enter password to access this page:");
